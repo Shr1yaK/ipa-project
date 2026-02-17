b4eaf76f37ac53719a8abbf5462fbb1107b133a3
prathish ghosh <prathish@prathishs-MacBook-Air.local>
2026-02-18 00:06:38 +0530

    Added pc, reg_file along with their tbs

--- PC.v
+++ PC.v
@@ -1,4 +1,18 @@
 module pc(
     input clk,
-)
+    input reset,
+    input [63:0] pc_in,
+    output [63:0] pc_out
+);
+reg [63:0] pc_temp;
+always @(posedge clk or posedge reset) begin
+    if(reset) begin
+        pc_temp <= 64'b0;
+    end 
+    else 
+    begin
+        pc_temp <= pc_in;
+    end
+end
+assign pc_out = pc_temp;
 endmodule
