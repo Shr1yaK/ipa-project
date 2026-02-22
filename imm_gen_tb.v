2f0045368a5daed21429dc01d5819bb77838c3f8
Kavya Veer <kavyaveer12@gmail.com>
2026-02-22 19:12:11 +0530

    Update imm_gen_tb.v

--- imm_gen_tb.v
+++ imm_gen_tb.v
@@ -8,17 +8,22 @@ module tb_imm_gen;
     initial begin
         //addi x1, x2, 5 -> imm = 5
         instruction = 32'h00500093; #10;
-        $display("5: Result %h", imm);
+        $display("R-Type: 5: Result %h", imm);
 
         //addi x1, x2, -1 -> imm = FFFFFFFFFFFFFFFF
         instruction = 32'hfff00093; #10;
-        $display("-1: Result %h", imm);
+        $display("I-Type: -1: Result %h", imm);
 
         //beq
         // [31] [30:25] [24:20] [19:15] [14:12] [11:8] [7] [6:0]
         instruction = 32'hfe000ce3; #10;
-        $display("beq: Result %h", imm);
+        $display("B-type: Result %h", imm);
+
+        //sd x1, 8(x2) -> imm 8
+        instruction = 32'h00113423; #10;
+        $display("S-type: 8: Result %h", imm);
     end
 
 endmodule
+
 
