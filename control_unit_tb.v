9b9e5f6df77501bfadb08cca17ef990680bdd769
Kavya Veer <kavyaveer12@gmail.com>
2026-02-22 19:32:53 +0530

    Update control_unit_tb.v

--- control_unit_tb.v
+++ control_unit_tb.v
@@ -17,21 +17,35 @@ module tb_control_unit;
     );
 
     initial begin
-        $monitor("Time: %0t | Op: %b | RegW: %b | ALUSrc: %b | ALUOp: %b", $time, opcode, RegWrite, ALUSrc, ALUOp);
-        
-        //add sub and or
+        $display("Opcode\t| Type\t| RegW\t| ASrc\t| MtoR\t| MRead\t| MWrite| Brch\t| ALUOp");
+        $display("-------------------------------------------------------------------------------");
+
+        //and sub add or
         opcode = 7'b0110011; #10;
+        $display("%b\t| R\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b", opcode, RegWrite, ALUSrc, MemtoReg, MemRead, MemWrite, Branch, ALUOp);
+
         //addi
         opcode = 7'b0010011; #10;
+        $display("%b\t| I\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b", opcode, RegWrite, ALUSrc, MemtoReg, MemRead, MemWrite, Branch, ALUOp);
+
         //ld
         opcode = 7'b0000011; #10;
+        $display("%b\t| LD\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b", opcode, RegWrite, ALUSrc, MemtoReg, MemRead, MemWrite, Branch, ALUOp);
+
         //sd
         opcode = 7'b0100011; #10;
+        $display("%b\t| SD\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b", opcode, RegWrite, ALUSrc, MemtoReg, MemRead, MemWrite, Branch, ALUOp);
+
         //beq
         opcode = 7'b1100011; #10;
+        $display("%b\t| BEQ\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b", opcode, RegWrite, ALUSrc, MemtoReg, MemRead, MemWrite, Branch, ALUOp);
+
         //null
-        opcode = 7'b0000000; #10; 
-        
+        opcode = 7'b0000000; #10;
+        $display("%b\t| NULL\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b\t| %b", opcode, RegWrite, ALUSrc, MemtoReg, MemRead, MemWrite, Branch, ALUOp);
+
+        $display("-------------------------------------------------------------------------------");
         $finish;
     end
+
 endmodule
